3b6816b30c27e86c5db3c91844267f353087763e
SieuwkeSheta <sieuwke_sheta@hotmail.nl>
2025-10-29 16:07:28 +0100

    Hamburgermenu class toegevoegd in JS en fout verbeterd in code close #16

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,11 @@ const hamMenuButton = document.querySelector(".ham-menu-button")
 const Blur = document.querySelector(".blur")
 
 hamMenuButton.addEventListener("click", toggleHamMenu)
-Blur.addEventListener("click, toggleHamMenu")
+Blur.addEventListener("click", toggleHamMenu)
 
 
 function toggleHamMenu() {
+    hamMenuButton.classList.toggle("is-open")
     hamMenu.classList.toggle("is-open")
     Blur.classList.toggle("is-open")
 }
